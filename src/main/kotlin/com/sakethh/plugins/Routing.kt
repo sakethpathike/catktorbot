57996f6d59230958cf7ae976c465bb67a7370d79
SakethPathike <sakethpathike@gmail.com>
2022-10-27 16:07:55 +0530

    few minor changes

--- src/main/kotlin/com/sakethh/plugins/Routing.kt
+++ src/main/kotlin/com/sakethh/plugins/Routing.kt
@@ -2,12 +2,19 @@ package com.sakethh.plugins
 
 import com.sakethh.api.apiRoutings
 import com.sakethh.bot.botRoutings
+import io.ktor.http.*
 import io.ktor.server.routing.*
 import io.ktor.server.application.*
+import io.ktor.server.response.*
+import io.ktor.util.reflect.*
+import java.io.File
 
 fun Application.configureRouting() {
     routing {
         apiRoutings()
         botRoutings()
+        get("/"){
+            call.respondRedirect("https://github.com/sakethpathike/catktorbot")
+        }
     }
 }
